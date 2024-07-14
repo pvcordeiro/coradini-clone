ff4473660d39c494a29d1b761718210a1992c594
pvcordeiro <pv.35@hotmail.com>
2024-07-14 19:27:31 +0100

    fixed eye button

--- login.js
+++ login.js
@@ -2,27 +2,26 @@ function toggleMode() {
     const html = document.documentElement;
     const isToggleModeActive = html.classList.toggle("toggle");
 
-    const eyeElement = document.querySelector(".eye");
-    const isOpen = eyeElement.classList.contains("eyeToggle");
-
-    if (isToggleModeActive) {
-        eyeElement.style.backgroundImage = isOpen
-            ? "var(--eye-open-color-dark)"
-            : "var(--eye-closed-color-dark)";
-    } else {
-        eyeElement.style.backgroundImage = isOpen
-            ? "var(--eye-open-color-light)"
-            : "var(--eye-closed-color-light)";
-    }
+    updateEyeStyle();
 }
 
 function toggleEye() {
     const eyeElement = document.querySelector(".eye");
     eyeElement.classList.toggle("eyeToggle");
 
+    updateEyeStyle();
+
+    const passwordInput = document.querySelector(".passwordInput");
+    passwordInput.type = eyeElement.classList.contains("eyeToggle")
+        ? "text"
+        : "password";
+}
+
+function updateEyeStyle() {
+    const eyeElement = document.querySelector(".eye");
+    const isOpen = eyeElement.classList.contains("eyeToggle");
     const html = document.documentElement;
     const isToggleModeActive = html.classList.contains("toggle");
-    const isOpen = eyeElement.classList.contains("eyeToggle");
 
     if (isToggleModeActive) {
         eyeElement.style.backgroundImage = isOpen
@@ -32,11 +31,5 @@ function toggleEye() {
         eyeElement.style.backgroundImage = isOpen
             ? "var(--eye-open-color-light)"
             : "var(--eye-closed-color-light)";
-    }
-    const passwordInput = document.querySelector(".passwordInput");
-    if (isOpen) {
-        passwordInput.type = "text";
-    } else {
-        passwordInput.type = "password";
     }
 }
